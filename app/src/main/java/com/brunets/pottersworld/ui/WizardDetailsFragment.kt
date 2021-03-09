cde7d5e3234775d68f71d9a760e569241a42e51d
bruno-c-nogueira <Bruno-C.Nogueira@btgpactual.com>
2021-03-09 20:11:48 -0300

    trying animation

--- app/src/main/java/com/brunets/pottersworld/ui/WizardDetailsFragment.kt
+++ app/src/main/java/com/brunets/pottersworld/ui/WizardDetailsFragment.kt
@@ -1,14 +1,18 @@
 package com.brunets.pottersworld.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AlphaAnimation
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import com.brunets.pottersworld.R
 import com.brunets.pottersworld.utils.loadImage
 import kotlinx.android.synthetic.main.fragment_wizard_details.*
+
 
 class WizardDetailsFragment : Fragment() {
     private val args: WizardDetailsFragmentArgs by navArgs()
@@ -23,11 +27,44 @@ class WizardDetailsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+
         arguments?.let {
             wizard_detail_name.text = args.wizardName
             wizard_detail_age.text = "Age: ${args.wizardAge}"
             wizard_detail_description.text = args.wizardDescription
             wizard_detail_photo.loadImage(args.wizardPhoto)
+            wizard_detail_photo.setOnClickListener {
+                it.blink()
+            }
+            wizard_detail_name.blink(2)
+            wizard_detail_name.setOnClickListener {
+                it.blink(3)
+            }
+
         }
+    }
+
+    fun View.rotateUp() {
+        animate().apply {
+            rotationX(360f)
+            duration = 1000
+        }.start()
+    }
+
+    fun View.blink(
+        times: Int = Animation.INFINITE,
+        duration: Long = 500L,
+        offset: Long = 100L,
+        minAlpha: Float = 0.1f,
+        maxAlpha: Float = 1.0f,
+        repeatMode: Int = Animation.REVERSE
+    ) {
+        startAnimation(AlphaAnimation(minAlpha, maxAlpha).also {
+            it.duration = duration
+            it.startOffset = offset
+            it.repeatMode = repeatMode
+            it.repeatCount = times
+        })
     }
 }
